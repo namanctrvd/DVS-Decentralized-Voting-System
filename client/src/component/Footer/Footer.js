9115ec84623d52003a3b578c2c79556fd0b8f137
Naman <naman19102@iiitnr.edu.in>
2022-05-15 15:34:43 +0530

    uploading all files

---
+++ client/src/component/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./Footer.css";
+
+const Footer = () => (
+  <>
+    <div className="footer-block"></div>
+    <div className="footer">
+      <div className="footer-container">
+        <p>
+          Group Project: Secured Blockchain Network.
+        </p>
+        <p>
+          By: Harsh, Naman, Prashant.
+        </p>
+      </div>
+    </div>
+  </>
+);
+
+export default Footer;
